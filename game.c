2725025ec6dd22654d285331f0daa81deaee1cd9
jkong <jkong@student.42seoul.kr>
2022-04-02 17:04:59 +0900

    잠깐 휴식겸 radix sort도 구경해보시죠? 하지만 만점이 아님을 곁들인...

--- game.c
+++ game.c
@@ -6,7 +6,7 @@
 /*   By: jkong <jkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 14:53:52 by jkong             #+#    #+#             */
-/*   Updated: 2022/04/02 15:41:16 by jkong            ###   ########.fr       */
+/*   Updated: 2022/04/02 17:04:10 by jkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,6 @@ static t_operation	_for_type(t_stack_type type, t_operation op)
 
 /*
  * Deprecated cause duplicated
- */
 static void	_do_game_2(t_game *game, t_stack_type type)
 {
 	const int		rev = type == OF_STACK_B;
@@ -46,6 +45,7 @@ static void	_do_game_2(t_game *game, t_stack_type type)
 		return ;
 	write_op(game, _for_type(type, SWAP));
 }
+ */
 
 		#include <stdio.h>
 		#include <stdarg.h>
@@ -60,75 +60,47 @@ static void	_do_game_2(t_game *game, t_stack_type type)
 			return dest;
 		}
 
-		static int __depth;
-
-static void	_partition(t_game *game, t_stack_type type, t_part *parent)
+static void	_partition(t_game *game, t_stack_type type, int level)
 {
-	const int	rev = type == OF_STACK_B;
-	t_part		child[STACK_TYPE_N];
 	size_t		i;
+	size_t		pop;
 
 	if (game->opt_visual)
 	{
-		__depth++;
-		visualize(__format("Partition Created. at %c [ %d ] ( %d, %d )", 'A' + (type == OF_STACK_B), __depth, parent->start, parent->length), game);
+		visualize(__format("Partition Created. at %c [ %d ]", 'A' + (type == OF_STACK_B), level), game);
 		getchar_safe();
 	}
-	child[0].start = parent->start;
-	child[0].length = parent->length / 2;
-	child[1].start = parent->start + child[0].length;
-	child[1].length = parent->length - child[0].length;
-	i = parent->length;
+	i = game->count[type];
+	pop = 0;
 	while (i-- > 0)
 	{
-		if (child[0].start <= game->stack[type]->rank && game->stack[type]->rank < child[0].start + child[0].length)
-			write_op(game, _for_type(_inverse(type), PUSH));
-		else
+		if ((game->stack[type]->rank >> level) & 1)
 			write_op(game, _for_type(type, ROTATE));
-	}
-	i = child[1].length;
-	if (i != game->count[type])
-	{
-		while (i-- > 0)
-			write_op(game, _for_type(type, REVERSE | ROTATE));
-	}
-	int k;
-	for (k = 0; k <= 1; k++)
-	{
-		i = child[k].length;
-		if (i > 2)
-			_partition(game, type ^ !k, &child[k]);
 		else
 		{
-			if (i >= 2)
-				_do_game_2(game, type ^ !k);
-			if (rev ^ !k)
-			{
-				while (i-- > 0)
-					write_op(game, PA);
-				i = child[k].length;
-			}
-			while (i-- > 0)
-				write_op(game, RA);
+			write_op(game, _for_type(_inverse(type), PUSH));
+			pop++;
 		}
 	}
+	i = pop;
+	while (i-- > 0)
+		write_op(game, _for_type(type, PUSH));
 	if (game->opt_visual)
 	{
-		visualize(__format("Partition Removed. at %c [ %d ] ( %d(%d | %d), %d(%d | %d) )", 'A' + (type == OF_STACK_B), __depth, parent->start, child[0].start, child[1].start, parent->length, child[0].length, child[1].length), game);
+		visualize(__format("Partition Removed. at %c [ %d ]", 'A' + (type == OF_STACK_B), level), game);
 		getchar_safe();
-		__depth--;
 	}
 }
 
 void	do_game(t_game *game)
 {
-	t_part	root;
+	int i;
 
 	if (is_sorted_stack_a(game) || do_game_mini(game))
 		return ;
-	root.start = 0;
-	root.length = game->length;
-	_partition(game, OF_STACK_A, &root);
+	i = 0;
+	while (game->length >> i)
+		_partition(game, OF_STACK_A, i++);
 	visualize("Not implemented. KO :(", game);
 }
 
